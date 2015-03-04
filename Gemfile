8df1f9b31cf8cac826274ce71b129f3636b6f212
Tim Guibord <timguibord@gmail.com>
2015-03-04 11:27:50 -0500

    js build logic

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in jedi.gemspec
 gemspec
+
+gem 'coffee-script', '~> 2.3.0'
+gem 'uglifier', '~> 2.7.1'
+gem 'sass', '~> 3.4.13'
+gem 'haml', '~> 4.0.6'
--- lib/jedi/cli.rb
+++ lib/jedi/cli.rb
@@ -7,6 +7,8 @@ module Jedi
   class CLI < Thor
     include Thor::Actions
 
+    JS_MATCHERS = "coffee, js"
+    CSS_MATCHERS = "scss, sass, css"
 
     def self.start(*)
       super
@@ -19,9 +21,37 @@ module Jedi
       base_dir = File.expand_path(File.dirname(__FILE__))
       FileUtils.mkdir name
       FileUtils.cp_r "#{base_dir}/template/.", name
-      puts "Hello #{name}"
     end
 
+    desc 'build', 'generates application and vendor JS files'
+    def build
+      build_js
+    end
+
+    private
+
+    def base_dir
+      Dir.pwd
+    end
+
+    def manifest
+      json = JSON.parse(File.read(File.join(base_dir, 'app', 'assets', 'assets.json')))
+      Struct.new(:js, :css).new(json['js'], json['css'])
+    end
+
+    def gather_assets(type, matchers, list)
+      list.map { |l| Dir.glob(File.join(base_dir, 'assets', type, "#{l}.{#{matchers}}")) }.flatten
+    end
+
+    def build_js
+      assets = gather_assets('javascript', JS_MATCHERS, manifest.js)
+      combined = assets.map do |asset|
+        src = File.read(asset)
+        CoffeeScript.compile(src) if File.split(asset).last.include? '.coffee'
+      end
+      compiled = Uglifier.compile(combined)
+      File.new(File.join(base_dir, 'build', 'javascript', "application-#{Time.now.iso8601}.js"), 'w').write(compiled)
+    end
 
   end
 end
